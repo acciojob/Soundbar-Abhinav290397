a55a785db317e92ba790a0cc6a701f5dedc28bab
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-12-10 20:28:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const Buttons = document.querySelectorAll(".btn");
+const StopBtn = document.querySelector(".stop");
+
+Buttons.forEach(x =>{
+	x.addEventListener("click",() =>{
+		const Audio = x.querySelector("audio");
+		Audio.play();
+	});
+});
+
+StopBtn.addEventListener("click",() =>{
+	const Audios = document.querySelectorAll("audio");
+	Audios.forEach(y,() =>{
+		y.pause();
+	});
+});
